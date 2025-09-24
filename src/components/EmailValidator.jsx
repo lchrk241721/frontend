5399f355074dd2dc0aafd2849e099e0b4edf3114
Chaitanya Rajkumar Limmala <lchrk2417@aol.com>
2025-09-24 20:46:23 +0530

    made some changes again

--- src/components/EmailValidator.jsx
+++ src/components/EmailValidator.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-//const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3001/api/email';
 
 const EmailValidator = ({ onValidationComplete, onValidationStart, onProgressUpdate, loading }) => {
   const [emails, setEmails] = useState('');
